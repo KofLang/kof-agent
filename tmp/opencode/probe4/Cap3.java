cbf63361b46cc3095e94b6c801bdec98b194e149
melissa <amelissariver@gmail.com>
2026-08-30 15:20:46 -0300

    chore: remove artefatos tmp

--- tmp/opencode/probe4/Cap3.java
+++
@@ -1,7 +0,0 @@
-public class Cap3 {
-    public static void main(String[] args) throws Exception {
-        Class<?> c = Class.forName("Default.Main");
-        java.lang.reflect.Method m = c.getDeclaredMethod("main");
-        m.invoke(null);
-    }
-}
